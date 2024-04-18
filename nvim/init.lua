a1142f18ea35c812fe28218afcb8429f1f42b3b8
Erik <81354428+X64X2@users.noreply.github.com>
2024-04-18 00:05:04 +0000

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require("base")
-require("highlights")
-require("keymaps")
-require("plugins")
-
-require("luasnip.loaders.from_vscode").lazy_load()
-
-vim.g.tokyonight_style = "night"
-vim.cmd("colorscheme tokyonight")
