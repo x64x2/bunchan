19662b3217b5e169eb4b9375a20e43d00d453099
Erik <81354428+x64x2@users.noreply.github.com>
2024-07-20 01:19:05 +0000

    merge conflict resolve

---
+++ admin.php
@@ -0,0 +1,15 @@
+<?php
+require 'foo.php';
+
+?><html>
+<head>
+	<title>kawaii- admin</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />  
+</head>
+<body>
+<h1>Admin</h1>
+<div> 
+<php?>
+</form>
+</body>
+</html> 
